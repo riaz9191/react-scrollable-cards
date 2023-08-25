aaaea37488fdc7d36c56f4d64751e2178f7603ee
riaz9191 <riazahammed9191@gmail.com>
2023-08-25 20:10:59 +0600

    scroll bar color

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { FaLayerGroup } from "react-icons/fa";
-import { FaCalendarDay,FaCalendarAlt } from "react-icons/fa"; 
-import { FaRocketchat } from "react-icons/fa6"; 
-import { GrAttachment } from "react-icons/gr"; 
+import { FaCalendarDay, FaCalendarAlt } from "react-icons/fa";
+import { FaRocketchat } from "react-icons/fa6";
+import { GrAttachment } from "react-icons/gr";
 import Post_card from "./Cards/Post_card";
 const Card = () => {
   return (
     <div className="card w-96 p-4  rounded-lg shadow-md">
       <div className="flex justify-between">
-        <h2>Incomplete</h2>
-        <p>0</p>
+        <div className="flex gap-2 items-center mb-4">
+          <div className="bg-[#D21010] p-4 rounded-tl-2xl rounded-bl-2xl"></div>
+          <h2>Incomplete</h2>
+        </div>
+        <p className="bg-[#E8EEF3] rounded-lg p-2 px-3 ">0</p>
       </div>
-      <div className="overflow-y-scroll max-h-screen w-80"> 
+      <div className="overflow-y-scroll max-h-screen w-80">
         <Post_card />
         <Post_card />
         <Post_card />
